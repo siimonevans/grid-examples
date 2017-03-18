1015a660cf5685e3321ecfa7db4f651d5195dbdd
Simon Evans <simon.evans@torchbox.com>
2017-03-18 23:35:45 +0000

    Update 404 page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
+import './NotFound.css'
 
 class NotFound extends Component {
   render() {
     return (
-      <div className="beer-item">
-        <h1>Not Found</h1>
+      <div className="wrapper not-found">
+        <h1>Page not found :(</h1>
+        <p>
+          Oh dear, it looks like this page no longer exists. <Link to='/'>Click here</Link> to go to the homepage.
+        </p>
       </div>
     )
   }
