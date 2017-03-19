dddd7350cec0f0f39d6ea931dbafc428e9106d79
Simon Evans <simon.evans@torchbox.com>
2017-03-19 12:35:03 +0000

    Update routes

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import Masonry3 from './components/Masonry/Masonry3'
 import Fluid from './components/Fluid/Fluid'
 import Responsive from './components/Responsive/Responsive'
 import Overlap from './components/Overlap/Overlap'
-import Overlap2 from './components/Overlap/Overlap2'
 import ExplicitlyPlaced from './components/ExplicitlyPlaced/ExplicitlyPlaced'
 import NegativeSpace from './components/NegativeSpace/NegativeSpace'
 import FlexibleBreadth from './components/FlexibleBreadth/FlexibleBreadth'
 import NamedTracks from './components/NamedTracks/NamedTracks'
+import DOMOrder from './components/DOMOrder/DOMOrder'
 import NotFound from './components/NotFound/NotFound'
 import './css/App.css'
 
@@ -37,11 +37,11 @@ class App extends Component {
             <Route path='Fluid' component={Fluid} />
             <Route path='Responsive' component={Responsive} />
             <Route path='Overlap' component={Overlap} />
-            <Route path='Overlap2' component={Overlap2} />
             <Route path='ExplicitlyPlaced' component={ExplicitlyPlaced} />
             <Route path='NegativeSpace' component={NegativeSpace} />
             <Route path='FlexibleBreadth' component={FlexibleBreadth} />
             <Route path='NamedTracks' component={NamedTracks} />
+            <Route path='DOMOrder' component={DOMOrder} />
 
             // 404
             <Route path='*' component={NotFound} />
